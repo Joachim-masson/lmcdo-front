0286c3a21d81389b5ccf5add0c36747e38971d4d
Joachim MASSON <joachim.masson.17@gmail.com>
2026-04-29 15:38:46 +0200

    start building of router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
 	return (
 		<nav className="navbar">
 			<h2> Component Navbar</h2>
-			<NavLink to="/home">Accueil</NavLink>
+			<NavLink to="/">Accueil</NavLink>
 			<NavLink to="/characters">Personnages</NavLink>
 			<NavLink to="/userManager">Gestion</NavLink>
 			<NavLink to="/connexion">Sing In</NavLink>
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,54 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import { StrictMode } from "react";
 import "./index.css";
 import App from "./App.tsx";
 
-const rootElement = document.getElementById("root");
+// Import the main app component
+import Home from "./pages/Home.tsx";
+import Error404 from "./pages/Error404.tsx";
+import Characters from "./pages/Characters.tsx";
+import Connexion from "./pages/Connexion.tsx";
+import UserManager from "./pages/UserManager.tsx";
 
+const router = createBrowserRouter([
+	{
+		element: <App />, // Renders the App component for the home page
+		children: [
+			{
+				path: "/characters",
+				element: <Characters />,
+			},
+			{
+				path: "/connexion",
+				element: <Connexion />,
+			},
+			{
+				path: "/",
+				element: <Home />,
+				errorElement: <Error404 />,
+			},
+			{
+				path: "/userManager",
+				element: (
+					//					<ProtectedRoute requiredRole="fullAdmin">
+					<UserManager />
+					//					</ProtectedRoute>
+				),
+			},
+		],
+	},
+]);
+
+// Find the root element in the HTML document
+const rootElement = document.getElementById("root");
 if (rootElement == null) {
 	throw new Error(`Your HTML Document must contain a <div id="root"></div>`);
 }
 
+// Render the app inside the root element
 createRoot(rootElement).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
