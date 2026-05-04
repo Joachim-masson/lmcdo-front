6e3bd8dd650525d12b02654af3a5444f769caf7b
Joachim MASSON <joachim.masson.17@gmail.com>
2026-05-04 12:21:42 +0200

    begin the css for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router";
 
+import "./Navbar.css";
+
 export default function Navbar() {
 	return (
 		<nav className="navbar">
-			<h2> Component Navbar</h2>
 			<NavLink to="/">Accueil</NavLink>
-			<NavLink to="/characters">Personnages</NavLink>
-			<NavLink to="/userManager">Gestion</NavLink>
-			<NavLink to="/connexion">Sing In</NavLink>
+			<div className="secondNavbar">
+				<NavLink to="/">Accueil</NavLink>
+				<NavLink to="/characters">Personnages</NavLink>
+				<NavLink to="/userManager">Gestion</NavLink>
+				<NavLink to="/connexion">Sing In</NavLink>
+			</div>
 		</nav>
 	);
 }
