b93b7d44fd1516ceedf31a91bc1c456366301589
Joachim MASSON <joachim.masson.17@gmail.com>
2026-05-13 12:11:44 +0200

    add security with sign in out and display or not management page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,36 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 
 import "./Navbar.css";
 
 export default function Navbar() {
+	const navigate = useNavigate();
+	// On vérifie si le token existe
+	const isLoggedIn = !!localStorage.getItem("userToken");
+
+	const handleAuthClick = () => {
+		if (isLoggedIn) {
+			// Si loggué, on déconnecte
+			localStorage.clear();
+			navigate("/"); // Redirection vers l'accueil après déconnexion
+		} else {
+			// Si pas loggué, on va vers la page de connexion
+			navigate("/connexion");
+		}
+	};
 	return (
 		<nav className="navbar">
 			<NavLink to="/">Accueil</NavLink>
 			<div className="secondNavbar">
 				<NavLink to="/">Accueil</NavLink>
 				<NavLink to="/characters">Personnages</NavLink>
-				<NavLink to="/userManager">Gestion</NavLink>
-				<NavLink to="/connexion">Sing In</NavLink>
+				{isLoggedIn && <NavLink to="/userManager">Gestion</NavLink>}
+				<button
+					onClick={handleAuthClick}
+					className="auth-nav-link"
+					type="button"
+				>
+					{isLoggedIn ? "Sign Out" : "Sign In"}
+				</button>
 			</div>
 		</nav>
 	);
